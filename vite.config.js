df09d4e2e70fc9c4009b5fd3c1d183cb5ab771cc
SulaimanTahaSantos <seltaha@sistemasmedioambientales.com>
2025-03-05 18:18:29 +0100

    hecho

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-    root: path.resolve(__dirname, 'src'),
+    root: path.resolve(__dirname),  // Cambiar 'src' a la raíz del proyecto
     publicDir: path.resolve(__dirname, 'public'),
     resolve: {
         alias: {
